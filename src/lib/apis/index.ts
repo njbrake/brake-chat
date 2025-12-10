26b980639518e7822cf241fdfc8ca65ea2d9f113
Nathan Brake <nathan@mozilla.ai>
2025-12-10 14:03:37 -0500

    remove changelog

--- src/lib/apis/index.ts
+++ src/lib/apis/index.ts
@@ -1375,32 +1375,6 @@ export const getBackendConfig = async () => {
 	return res;
 };
 
-export const getChangelog = async () => {
-	let error = null;
-
-	const res = await fetch(`${WEBUI_BASE_URL}/api/changelog`, {
-		method: 'GET',
-		headers: {
-			'Content-Type': 'application/json'
-		}
-	})
-		.then(async (res) => {
-			if (!res.ok) throw await res.json();
-			return res.json();
-		})
-		.catch((err) => {
-			console.error(err);
-			error = err;
-			return null;
-		});
-
-	if (error) {
-		throw error;
-	}
-
-	return res;
-};
-
 export const getVersion = async (token: string) => {
 	let error = null;
 
