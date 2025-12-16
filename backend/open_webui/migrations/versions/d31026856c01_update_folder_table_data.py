647cd8d347ddfbf3fde3faea3c22a712110390ef
Nathan Brake <33383515+njbrake@users.noreply.github.com>
2025-12-16 13:29:53 -0500

    chore: update down_revision in folder table migration (#31)
    - Changed down_revision from 9f0c9cd09105 to 3781e22d8b01 to reflect the latest migration dependency.

--- backend/open_webui/migrations/versions/d31026856c01_update_folder_table_data.py
+++ backend/open_webui/migrations/versions/d31026856c01_update_folder_table_data.py
@@ -1,7 +1,7 @@
 """Update folder table data
 
 Revision ID: d31026856c01
-Revises: 9f0c9cd09105
+Revises: 3781e22d8b01
 Create Date: 2025-07-13 03:00:00.000000
 
 """
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 from alembic import op
 
 revision = "d31026856c01"
-down_revision = "9f0c9cd09105"
+down_revision = "3781e22d8b01"
 branch_labels = None
 depends_on = None
 
